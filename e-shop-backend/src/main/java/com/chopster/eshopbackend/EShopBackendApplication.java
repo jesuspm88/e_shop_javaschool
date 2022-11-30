20619e2c9c7059a3bb227b390861a51927364b6f
Jesús Pérez <112932138+jesuspm88@users.noreply.github.com>
2022-11-30 12:03:41 +0100

    Update 30/11 3
    Finally solved running errors, now tables are not being created and no errors are given.

--- e-shop-backend/src/main/java/com/chopster/eshopbackend/EShopBackendApplication.java
+++ e-shop-backend/src/main/java/com/chopster/eshopbackend/EShopBackendApplication.java
@@ -2,7 +2,10 @@ package com.chopster.eshopbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -10,6 +13,9 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.Arrays;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.chopster.eshopbackend.model")
+@ComponentScan(basePackages = { "com.chopster.eshopbackend.model" })
+@EntityScan("com.chopster.eshopbackend.model")
 public class EShopBackendApplication {
 
 	public static void main(String[] args) {
